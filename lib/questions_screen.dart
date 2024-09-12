0bb0db233f9bc385298a262723001c3bf93d1766
Richard <rdobmk@gmail.com>
2024-09-12 10:14:25 +0200

    Add Google fonts to the app

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'package:quizz_app/answer_button.dart';
 import 'package:quizz_app/data/questions.dart';
@@ -38,8 +39,10 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           children: [
             Text(
               currentQuestion.text,
-              style: const TextStyle(
-                color: Colors.white,
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 201, 153, 251),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
               textAlign: TextAlign.center,
             ),
